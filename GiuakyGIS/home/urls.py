814e5c1a2839f1d0d497172a8cb35f9644d039c6
DuongHuy25 <1250080068@sv.hcmunre.edu.vn>
2026-02-05 19:03:00 +0700

    HTML và Tool

--- GiuakyGIS/home/urls.py
+++ GiuakyGIS/home/urls.py
@@ -2,6 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Đường dẫn cho trang chủ (để trống chuỗi đầu tiên đại diện cho trang gốc)
+    # Trang chủ
     path('', views.home_view, name='home'),
+    
+    # Các đường dẫn món ăn
+    path('do-uong/', views.do_uong_view, name='do_uong'),
+    path('ga-chien/', views.ga_chien_view, name='ga_chien'),
+    path('hamburger/', views.hamburger_view, name='hamburger'),
+    path('khoai-tay/', views.khoai_tay_view, name='khoai_tay'),
+    path('pizza/', views.pizza_view, name='pizza'),
+    
+    # Các đường dẫn chức năng
+    path('gio-hang/', views.gio_hang_view, name='gio_hang'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('map/', views.map_view, name='map'),
+    path('tinh-trang/', views.tinh_trang_view, name='tinh_trang'),
 ]
--- GiuakyGIS/home/views.py
+++ GiuakyGIS/home/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 
+# --- Trang chủ ---
 def home_view(request):
-    # Django sẽ tự tìm trong thư mục Templates của bạn
     return render(request, 'home.html')
+
+# --- Nhóm thực đơn (Menu) ---
+def do_uong_view(request):
+    return render(request, 'do-uong.html')
+
+def ga_chien_view(request):
+    return render(request, 'ga-chien.html')
+
+def hamburger_view(request):
+    return render(request, 'hamburger.html')
+
+def khoai_tay_view(request):
+    return render(request, 'khoai-tay.html')
+
+def pizza_view(request):
+    return render(request, 'pizza.html')
+
+# --- Nhóm chức năng & Tiện ích ---
+def gio_hang_view(request):
+    # Django phân biệt hoa thường, file của bạn là 'Gio-hang.html'
+    return render(request, 'Gio-hang.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
+
+def map_view(request):
+    return render(request, 'map.html')
+
+def tinh_trang_view(request):
+    # File của bạn là 'Tinhtrang.html'
+    return render(request, 'Tinhtrang.html')
